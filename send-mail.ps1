232d3fb47d1eaebcf916af4505b71bfba5d6a520
catpele <cdavis.home@gmail.com>
2017-05-04 06:47:49 +0300

    Update send-mail.ps1

--- send-mail.ps1
+++ send-mail.ps1
@@ -3,5 +3,6 @@ $SmtpUser = 'user@outlook.com'
 $smtpPassword = 'P@ssw0rd'
 $MailtTo = 'mike@viamonstra.com'
 $MailFrom = 'user@outlook.com'
+$emailSmtpServerPort = '587'
 $MailSubject = "Test using $SmtpServer" $Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $SmtpUser, $($smtpPassword | ConvertTo-SecureString -AsPlainText -Force) 
-Send-MailMessage -To "$MailtTo" -from "$MailFrom" -Subject $MailSubject -SmtpServer $SmtpServer -UseSsl -Credential $Credentials
+Send-MailMessage -To "$MailtTo" -from "$MailFrom" -Subject $MailSubject -SmtpServer $SmtpServer -UseSsl -Port $emailSmtpServerPort -Credential $Credentials
